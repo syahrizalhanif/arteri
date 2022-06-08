56764ffa895ed5161d40e7e25182d170cb371872
Rizal <syahrizalhanif@gmail.com>
2022-06-08 11:09:50 +0700

    Pretty URL

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SE
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
